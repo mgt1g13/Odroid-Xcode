3645c665c11af5e8e40eeb4fab23be7bb2777615
Matheus Trindade <matheustrindade@MacBook-Air-de-Matheus.local>
2015-09-10 15:17:03 -0300

    Initial Commit

---
+++ OdroidXcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OdroidXcode
+//
+//  Created by Matheus Trindade on 10/09/15.
+//  Copyright (c) 2015 Matheus Trindade. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
